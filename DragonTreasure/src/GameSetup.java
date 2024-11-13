edb1867aa64fe94425ad5bae06fd6b9683990b30
Martino <60008678+Martin-Tule@users.noreply.github.com>
2024-11-13 18:42:05 +0100

    added logic for holding door descriptors.

--- DragonTreasure/src/GameSetup.java
+++ DragonTreasure/src/GameSetup.java
@@ -14,14 +14,13 @@ public class GameSetup {
     }
 
     private void populateRoomDescriptions () {
-        roomDescriptions[0] = ("Du står utanför en grotta. Det luktar svavel från öppningen\r\n" + //
-                        "Grottöppningen är österut. Skriv \"ö\" och tryck på [Enter] för att komma in i grottan");
-        roomDescriptions[1] = ("När du går in i grottan kollapsar ingången bakom dig.\r\n" + //
+        roomDescriptions[0] = ("Du står utanför en grotta. Det luktar svavel från öppningen\r\n");
+        roomDescriptions[1] = ("När du går in i grottan kollapsar ingången bakom dig.\r\n" + 
                         "Rummet är upplyst av några ljus som sitter på ett bord framför dig.");
         roomDescriptions[2] = ("Du ser en död kropp på golvet.");
         roomDescriptions[3] = ("Du ser en brinnande fackla i rummets ena hörn och känner en motbjudande stank.");
         roomDescriptions[4] = ("Du kommer in i ett fuktigt rum med vatten sipprandes längs den västra väggen.");
-        roomDescriptions[5] = ("Du kommer in i ett rymligt bergrum med en ljusstrimma sipprandes genom en spricka i\r\n" + //
+        roomDescriptions[5] = ("Du kommer in i ett rymligt bergrum med en ljusstrimma sipprandes genom en spricka i\r\n" + 
                         "den östra väggen.");
         roomDescriptions[6] = ("");
         roomDescriptions[7] = ("Du lämnar grottan med livet i behåll. Grattis, du förlorade inte!");
@@ -30,7 +29,8 @@ public class GameSetup {
     // create the rooms in the dungeon with related doors.
     private Room createRoom0() {
         // create doors for the room firstly.
-        Door doorRoom0East = new Door('ö', false, 1);
+        Door doorRoom0East = new Door('ö', false, 1, 
+        "Grottöppningen är österut. Skriv \"ö\" och tryck på [Enter] för att komma in i grottan");
         doors = new Door[1];
         doors[0] = doorRoom0East; // store in array as that is expected arg.
         // create the room with the created doors as args.
@@ -38,8 +38,10 @@ public class GameSetup {
         return room0;
     }
     private Room createRoom1() {
-        Door doorRoom1North = new Door('n', false,2);
-        Door doorRoom1South = new Door('s', false, 5);
+        Door doorRoom1North = new Door('n', false,2,
+        "Du kan gå norrut [n]");
+        Door doorRoom1South = new Door('s', false, 5,
+        "Du kan gå söderut [s]");
         doors = new Door[2];
         doors[0] = doorRoom1North;
         doors[1] = doorRoom1South;
