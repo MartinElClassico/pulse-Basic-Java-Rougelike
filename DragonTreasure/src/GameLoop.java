855162c419057bf9956e6d9bbf042f365071bcdb
Martino <60008678+Martin-Tule@users.noreply.github.com>
2024-12-03 23:58:23 +0100

    Revert "Gameloop comments edit2"
    This reverts commit 6f46a049ae5bebb91a19ee299c1022ed0f28e4b7.

--- DragonTreasure/src/GameLoop.java
+++ DragonTreasure/src/GameLoop.java
@@ -124,33 +124,19 @@ public class GameLoop {
      * @return an array of characters with valid player movement prompts for the current room's doors.
      */
     private char[] validDirectionsInRoom() {
-        /** 
-         * Create list to store valid directions as characters.
-         */
+        // create list of characters to store valid directions.
         List<Character> validDirectionsList = new ArrayList<Character>();
-        /**
-         * Retrieve all door objects in the current room.
-         */
+        // all door objects in the current room.
         Door[] allDoorsInRoom = this.currentRoom.getDoors();
-        /**
-         * Loop to get directions based on the current doors in the room.
-         * Add doors and their respective positions in list.
-         * Door positions are retrieved by getPosition and added to validDirectionList.
-         */
+        // store all door positions of room in list.
         for (Door doorInRoom : allDoorsInRoom) {
             validDirectionsList.add(doorInRoom.getPosition());
         }
-        /**
-         * Create char array to store valid directions
-         */
+        // create char array to store list elements inside of array instead of list.
         char[] validDirectionArray = new char[validDirectionsList.size()];
-        /**
-         * store each element from list in array.
-         */
+        // store each element from list in array.
         for (int i = 0; i<validDirectionsList.size(); i++){
-        /**
-         * convert each list element of type Object to char type and store in array.
-         */
+            // convert each list element of type Object to char type and store in array.
             validDirectionArray[i] = (char)validDirectionsList.get(i);
         }
         return validDirectionArray;
