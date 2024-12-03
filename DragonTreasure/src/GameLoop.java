d8c7e50e8a79817ecb909481db2f31e188c1ce70
Martino <60008678+Martin-Tule@users.noreply.github.com>
2024-12-03 12:37:53 +0100

    commented all the code in class.

--- DragonTreasure/src/GameLoop.java
+++ DragonTreasure/src/GameLoop.java
@@ -8,68 +8,110 @@ import utils.InputHandler;
  * Holds the main gameplay.
  */
 public class GameLoop {
+
     private Scanner sc; // Scanner for reading user input throughout the game
-    private static String welcomeMessage = ("Välkommen till Dragon Treasure\r\n" + 
+    /**
+     * holds a static final welcome message to print to player.
+     */
+    private static final String welcomeMessage = ("Välkommen till Dragon Treasure\r\n" + 
     "Skriv ditt namn och tryck på [Enter] för att starta ett nytt spel...");
-    private static String quitMessage = "Du lämnar spelet!";
+    /**
+     * holds a static final quit message to print to player upon quitting game.
+     */
+    private static final String quitMessage = "Du lämnar spelet!";
+    /**
+     * holds the player class, is not used in current (part 1) implementation of the game.
+     */
     private Player player;
+    /**
+     * holds all the room classes that together make up the dungeon except for the player.
+     */
     private Room[] dungeonRooms;
+    /**
+     * boolean value so the method can keep track of if gameloop is running or not (e.g. paused - not yet implemented)
+     */
     private boolean running = true; //so that the loop can be quit from outside.
      /**
      * private instance to hold the current room that the player is in.
      */
     private Room currentRoom;
 
-/**
- * Constructor to initialise the gameloop with an Array containing all rooms in the dungeon.
- * @param dungeonRooms Array containing all rooms in the dungeon.
- */
+    /**
+     * Constructor to initialise the gameloop with an Array containing all rooms in the dungeon.
+     * @param dungeonRooms Array containing all rooms in the dungeon.
+     */
     public GameLoop(Room[] dungeonRooms) {
         this.sc = new Scanner(System.in);
         this.dungeonRooms = dungeonRooms;
     }
-// prints welcome message to player.
+
+    /**
+     * prints welcome message to player.
+     */
     private void printWelcomeMsg() {
         System.out.println(welcomeMessage);
     }
-// fetches players name from user input
+
+    /**
+     * fetches players name from user input.
+     * @return a string of the unsanitized user input
+     */
     private String fetchPlayerName() {
-        // create scanner to get input. 
         // get user rawInput
-        // TODO: check if we need a loop here to control user input or not. 
+        // TODO: check if we need logic here to control user input or not. 
         // fetches user input and trims white characters.
         String userInput = sc.nextLine().trim();
         return userInput;
     }
 
-    // print welcomemessage to user:
+    /**
+     * prints personalized welcome message to user
+     * @param playerName a string with the name of the player.
+     */
     private void printWelcomePlayer(String playerName) {
         System.out.printf("Välkommen %s till din skattjakt.%n", playerName);
     }
-
-    // TODO: motivate if this should be public or private with a help method (public).
-    // Note: keep public in case called on externally.
-    // prompt player to enter username and returns it as a String.
     
+    /**
+     * main logic for getting username from player:
+     * 1. prints welcome message to the game and input prompt.
+     * 2. gets player name.
+     * 3. prints personal welcome message to the player
+     * @return a string with the name of the player.
+     */
     private String promptUserName() {
         printWelcomeMsg();
         String playerName = fetchPlayerName();
         printWelcomePlayer(playerName);
         return playerName;
     }
-// create a new player using the returned name from promptUserName().
+
+    /**
+     * create a new player using the returned name from promptUserName().
+     * the name is accessed via a private class atttribute.
+     */
     private void getAndSetNewPlayer() {
         this.player = new Player(promptUserName());
     }
 
+    /**
+     * get status of loop from outside class, is the gameloop running?
+     * @return boolean true if loop is running, false if paused or terminated.
+     */
     public boolean getRunning() {
         return this.running;
     }
-// prints the description of the current room.
+
+    /**
+     * prints the description of the current room. The current room is accesed via private class attribute.
+     */
     private void printRoomDesc(){
         System.out.println(this.currentRoom.getRoomDesc());
     }
-// get description of all doors in current room.
+
+    /**
+     * Prints description of all doors in current room. The current room is accesed via private class attribute.
+     */
     private void printAllDoorsDescriptions(){
         Door[] allDoorsInRoom = this.currentRoom.getDoors();
         for (Door doorInRoom : allDoorsInRoom) {
@@ -77,7 +119,10 @@ public class GameLoop {
         }
     }
     
-// return an Array with valid directions (doors) in current room
+    /**
+     * Gets an array with all the directions the player can move based on the current room the player is in.
+     * @return an array of characters with valid player movement prompts for the current room's doors.
+     */
     private char[] validDirectionsInRoom() {
         // create list of characters to store valid directions.
         List<Character> validDirectionsList = new ArrayList<Character>();
@@ -97,6 +142,15 @@ public class GameLoop {
         return validDirectionArray;
     }
 
+    /**
+     * Returns the door the user has chosen based on move direction chosen and the doors avaliable in the room.
+     * Has try-throw-catch logic to catch if an invalid roomdirection is chosen.
+     * This should never happen however as prior to this method call the users choice has been validated via the 
+     * InputHandler.java class.
+     * @param roomDirection Users chosen direction to move (has been through input handler to ensure valid direction)
+     * @param roomDoors all the doors avaliable in the current room the player moves from.
+     * @return a Door object instance that contains the chosen door.
+     */
     private Door fetchChoosenDoor(char roomDirection, Door[] roomDoors) {
         // try-catch in-case the if loop fails to return anything.
         try {
@@ -113,12 +167,19 @@ public class GameLoop {
         }
     }
 
-
+    /**
+     * this method starts the game loop and hold the logic related to said game loop.
+     * 
+     * @param startRoomID the room id that the loop should start from, is usually 0 at start.
+     * this does however make it possible for the game loop to be restarted at a different room based on a 
+     * not yet implemented save state or pause state.
+     */
     public void start(int startRoomID) {
-        InputHandler inpHand = new InputHandler();
-        getAndSetNewPlayer();
-        this.currentRoom = this.dungeonRooms[startRoomID];
+        InputHandler inpHand = new InputHandler(); // instance of class to make sure user input for direction is legal.
+        getAndSetNewPlayer(); //creates the new player and handles output and input terminal statements for that end goal.
+        this.currentRoom = this.dungeonRooms[startRoomID]; //set current room that the player starts in.
         boolean doorLocked = false; // if door is locked we should'nt print room description until an unlocked door is chosen.
+        // start of the loop the player moves in itterations through.
         while (running) {
         // base loop:
             // print room description, but only if an unlocked door was chosen previously:
@@ -129,27 +190,25 @@ public class GameLoop {
             printAllDoorsDescriptions();
             // get user input
             char userInp = inpHand.getUserInpMovement(validDirectionsInRoom());
-            // TODO: no logic for peeking through locked door (default move).
+            // if not q then it is a valid door direction.
+            if (userInp != 'q') {
+                // get the door the player has chosen to move through.
+                Door chosenDoor = fetchChoosenDoor(userInp, currentRoom.getDoors());
+                //if door is locked, print door peep 
+                if (chosenDoor.getLocked()) {
+                    doorLocked = true; // set door locked to true as to not print room desciption.
+                    System.out.println(chosenDoor.getKeyholeViewDescription());
+                } else {
+                    doorLocked = false; // needs to be set so room description is displayer if not locked.
+                    // else set the current room to the room the chosen door leads to.
+                    this.currentRoom = dungeonRooms[chosenDoor.getConnectedRoomID()];
+                }
+            }
             // exit condition1:
             // user choses 'q' and quits the game.
             if (userInp == 'q'){
                 System.out.println(quitMessage); 
                 this.running = false;
-            }
-            // if not q then it is a valid door direction.
-            if (userInp != 'q') {
-                doorLocked = false; // always reset locked status first.
-                // add logic here for if door is locked.
-                Door chosenDoor = fetchChoosenDoor(userInp, currentRoom.getDoors());
-                //if door is locked, print door peep 
-                if (chosenDoor.getLocked()) {
-                    doorLocked = true;
-                    System.out.println(chosenDoor.getKeyholeViewDescription());
-                } else {
-                    // else set the current room to the room the chosen door leads to.
-                    this.currentRoom = dungeonRooms[chosenDoor.getConnectedRoomID()];
-                }
-                //this.currentRoom = fetchNewRoom(userInp, currentRoom.getDoors());
             }
             // exit condition2:
             // user finds the dungeon exit.
@@ -159,12 +218,15 @@ public class GameLoop {
             }
             
         }    
+        //end of the loop the player moves in itteration through.
+        // check if win condition was reason we exited loop (could be because of death or quit)
         if (currentRoom.getRoomId() == 7) {
             // this correspond to win message.
             printRoomDesc();
         }
-        inpHand.closeScanner();
-        sc.close();
+        // close all scanners to handle input to avoid resource leaks.
+        inpHand.closeScanner(); //close scanner in method to handle checks on user input movement.
+        sc.close(); // close scanner used to handle creation of player.
     }
 
 
