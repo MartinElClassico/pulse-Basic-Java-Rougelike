963f810db691feb8bb5a404e87b79afd66703137
evelinaek <3velina.kholod@gmail.com>
2024-12-03 19:52:13 +0100

    hejmeddelande

--- DragonTreasure/src/GameLoop.java
+++ DragonTreasure/src/GameLoop.java
@@ -7,7 +7,7 @@ import utils.InputHandler;
  * The gameloop class handles the players interactions and room transitions.
  * Holds the main gameplay.
  */
-// merge conflict test2 
+// merge conflict test2 hejhej
 public class GameLoop {
 
     private Scanner sc; // Scanner for reading user input throughout the game
