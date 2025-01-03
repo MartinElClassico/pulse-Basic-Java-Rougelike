6baa2580ca9202e7c79e8973fdec304abb057e3e
MartinElClassico <mtul@pm.me>
2025-01-03 21:56:18 +0100

    implemented custom exception. logic for using inventory. Made a shared scanner closed from outside the class.

--- DragonTreasure/src/main/game/entities/Inventory.java
+++ DragonTreasure/src/main/game/entities/Inventory.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 import main.game.items.Item;
 import main.game.io.InputHandler;
+import main.game.entities.ItemNotFoundException;
 
 public class Inventory {
     /**
      * holds the items the player has picked up.
      */
     private List<Item> items;
+
+    private InputHandler inpHand = new InputHandler(); // Shared InputHandler instance
 
     /**
      * holds static final message to print when inventory is empty while trying to access it.
@@ -22,7 +25,14 @@ public class Inventory {
      */
     private static final String FIRSTLN_INV_MSG = "Your inventory contains:";
 
+    private static final String ITEM_SELECT_PROMPT = "Choose an item by pressing its number. Press 0 to return.";
+    
     private static final String NO_I_SELECTED_MSG = "No item selected! Returning from inventory.";
+
+    private static final char[] VIEW_ITEM_COMMANDS = {'v','u'};
+
+    private static final String ITEM_USE_PROMPT = "You selected %s. Press v to view it, u to use or q to go back.";
+
 
     /**
      * contructor, initalizes inventory.
@@ -42,7 +52,7 @@ public class Inventory {
     /**
      * removes a stored Item object from the inventory.
      * @param itemName the name of the object to be removed.
-     * @throws IllegalArgumentException if the item to be removed is not found, it will throw an error.
+     * @throws ItemNotFoundException if the item to be removed is not found, it will throw an error.
      */
     public void removeItem(String itemName) {
         boolean success = false;
@@ -54,7 +64,7 @@ public class Inventory {
             }
         }
         if (!success) {
-            throw new IllegalArgumentException("Item not found in inventory!");
+            throw new ItemNotFoundException("Item not found in inventory!");
         }
     }
 
@@ -62,7 +72,7 @@ public class Inventory {
      * fetches an item from the inventory.
      * @param itemName the name of the item to be fetched from the inventory.
      * @return foundItem the item found in the inventory.
-     * @throws IllegalArgumentException if the item to be fetched is not found, it will throw an error.
+     * @throws ItemNotFoundException if the item to be fetched is not found, it will throw an error.
      */
     public Item getItem(String itemName) {
         for (Item item: items) {
@@ -70,7 +80,7 @@ public class Inventory {
                 return item;
             }
         }
-        throw new IllegalArgumentException("Item not found in inventory!");
+        throw new ItemNotFoundException("Item not found in inventory!");
     }
 
     /**
@@ -86,18 +96,19 @@ public class Inventory {
             for (int i = 0; i < items.size(); i++) {
                 Item item = items.get(i);
                 // print out the items with index first (+1), so that player can access them through index later.
-                System.out.printf("%d.%t %s%n", i+1, item.getName());
+                System.out.printf("%d. %s%n", i+1, item.getName());
             }
             return true;
         }
         }
-
-    public Item accessInventory() {
+    
+    private Item accessInventory() {
         boolean hasItems = printInventory();
         if (hasItems) {
-            InputHandler inpHand = new InputHandler(); // instance of class to make sure input is legal.
-            int choice = inpHand.handleIntToken(0+1, items.size(), 0);
+            System.out.println(ITEM_SELECT_PROMPT);
             // handle user input and get user output.
+            int choice = inpHand.handleIntToken(0+1, items.size(), 0);
+            // act according to what the user chose to do:
             if (choice == 0) {
                 System.out.println(NO_I_SELECTED_MSG);
                 return null;
@@ -108,5 +119,54 @@ public class Inventory {
             return null;
         }
     }
+
+    public Item manageInventory() {
+        Item chosen_item = accessInventory();
+        // did not chose anything, so return null.
+        if (chosen_item == null) {
+            return null;
+        }
+        // show menu for what can be done with object and return to main.
+        // viz. view item (v) --> return null, or use item (u)--> return item:
+        char chosen_action = itemUseMenu(chosen_item);
+        if (chosen_action == 'q') {return null;} 
+        else {return chosen_item;}
+    }
+
+    private char itemUseMenu(Item chosenItem) {
+        // prints "you selected {item}" along with keypresses avaliable.
+        System.out.printf(ITEM_USE_PROMPT, chosenItem.getName());
+        char choice = inpHand.handleCharToken(VIEW_ITEM_COMMANDS, 'q');
+        switch (choice) {
+            case 'v':
+                viewItem(chosenItem);
+                return 'v';
+            case 'q':
+                return 'q';
+            default:
+                useItemPrompt(chosenItem);
+                return 'u';
+        }
+    }
+
+    private void useItemPrompt(Item item){
+        System.out.printf("You use %s!", item.getName());
+
+    }
+
+    private void viewItem(Item item) {
+        System.out.printf("%s - %s%n", item.getName(), item.getDescription());
+        System.out.println(item.getAsciiArt());
+    }
+
+    public void closeInput() {
+        inpHand.closeScanner(); // Close Scanner only once when done
+    }
+
+    // TODO: add useItem private method, perhaps should be in Player.java?
+
+    // TODO: make public method called "manageInventory" and make it so that it can use the items therein.
+    // alternatively, add this functionality to player.
+
 
 }
