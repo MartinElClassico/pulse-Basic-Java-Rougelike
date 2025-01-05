40170ebb9519c4f1d855c9619ceff6811789ad9e
evelinaek <3velina.kholod@gmail.com>
2025-01-05 09:50:01 +0100

    javadocs comments monster

--- DragonTreasure/src/main/game/entities/Monster.java
+++ DragonTreasure/src/main/game/entities/Monster.java
@@ -1,7 +1,13 @@
 package main.game.entities;
 
+/** 
+* This class represents a monster in the game that can take damage, do damage and get defeated
+* It handles both monster and dragon, differentiating based on the name of the monster
+*/
+
 public class Monster {
 
+// constants for messages
     private static final String ATTACKED_MONSTER_MESSAGE = "Du attackerar odjuret och gör %d skada %n";
     private static final String ATTACKED_DRAGON_MESSAGE = "Du attackerar draken och gör %d skada %n";
     private static final String ATTACK_MONSTER_MESSAGE = "Ett odjur attackerar dig och gör %d skada";
@@ -9,16 +15,21 @@ public class Monster {
     private static final String DEFEAT_MONSTER_MESSAGE = "Du besegrar odjutet";
     private static final String DEFEAT_DRAGON_MESSAGE = "Du besegrar draken";
 
-    private static final int DAMAGE = 1; //monster/drake skada är en konstant
+    private static final int DAMAGE = 1; // monster/dragon damage is a constant
 
-    private String name; //typ av monster
-    private int healthPoints;
-    private int maxHealthPoints;
-    private String monsterDesc;
-    private boolean defeated;
+    private String name; // differentiates between monster and dragon
+    private int healthPoints; // current health points
+    private int maxHealthPoints; // max health points
+    private String monsterDesc; // description of the monster
+    private boolean defeated; // whether the monster is defeated or not
 
-    
-    //constructor
+    /** 
+     * Constructor to create a monster with its name, max health points and description
+     * 
+     * @param name
+     * @param maxHealthpoints
+     * @param monsterDesc
+    */
     public Monster(String name, int maxHealthPoints, String monsterDesc) {
         this.name = name;
         this.healthPoints = maxHealthPoints;
@@ -27,34 +38,61 @@ public class Monster {
         this.defeated = false;
     }
 
+/**
+ * Gets the name of the monster
+ * @return The name of the monster
+ */
     public String getName() {
         return name;
     }
 
+/**
+ * Gets the current health points of the monster
+ * @return The current health points of the monster
+ */
     public int getHealthPoints() {
        return healthPoints;  
     }
 
+/**
+ * Gets the max health points of the monster
+ * @return The max health points of the monster
+ */
     public int getMaxHealthPoints() {
         return maxHealthPoints;
     }
 
+/**
+ * Gets the damage of the monster
+ * @return The damage of the monster
+ */
     public int getDamage() {
         return DAMAGE;
     }
 
+/**
+ * Gets the description of the monster
+ * @return The description of the monster
+ */
     public String getMonsterDesc() {
         return monsterDesc;
     }
 
-    public boolean isDefeated() [
+/**
+ * Checks if the monster is defeated
+ * @return true if the monster is defeated, otherwise false
+ */
+    public boolean isDefeated() {
         return defeated;
-    ]
+   }
 
-//metod för att hantera att spelaren tar skada
+/**
+ * Method for attacking the player
+ * @param player The player being attacked
+ */
 public void attackPlayer(Player player) {
     if (!defeated) {
-    player.takeDamage(DAMAGE); 
+    player.takeDamage(DAMAGE); // Calls Player class' takeDamage method
     if (name.equals("Monster")) {
         System.out.printf(ATTACK_MONSTER_MESSAGE, DAMAGE);
     } else {
@@ -63,7 +101,9 @@ public void attackPlayer(Player player) {
   }
 }
 
-// metod för att hantera att monster tar skada
+/**
+ * Method to handle the monster taking damage and mark as defeated when healthpoints reach 0
+ *  */
 public void takeDamage(int damage) {
     if (!defeated) {
         healthPoints = healthPoints - damage;
